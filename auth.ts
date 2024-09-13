e06792d4b174e5f8c61a3d27943ccf7ea2eeaa81
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-09-13 13:12:54 +0100

    added player

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
+// import NextAuth from "next-auth"
+// import Google from "next-auth/providers/google"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
-})
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [Google],
+// })
